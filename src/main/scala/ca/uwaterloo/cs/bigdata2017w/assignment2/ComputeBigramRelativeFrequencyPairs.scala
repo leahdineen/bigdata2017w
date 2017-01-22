08cf25b9920b084ee55ffe99112dd3b5c61c9f2f
Leah Dineen <leahmd23@gmail.com>
2017-01-22 14:05:08 -0500

    bigram relative frequency

---
+++ src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment2/ComputeBigramRelativeFrequencyPairs.scala
@@ -0,0 +1,66 @@
+package ca.uwaterloo.cs.bigdata2017w.assignment2
+
+import io.bespin.scala.util.Tokenizer
+import io.bespin.scala.util.WritableConversions
+
+
+import org.apache.log4j._
+import org.apache.hadoop.fs._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+
+import tl.lin.data.pair.PairOfStrings
+
+
+class ConfPairs(args: Seq[String]) extends ScallopConf(args) with Tokenizer{
+  mainOptions = Seq(input, output, reducers)
+  val input = opt[String](descr = "input path", required = true)
+  val output = opt[String](descr = "output path", required = true)
+  val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
+  verify()
+}
+object ComputeBigramRelativeFrequencyPairs extends Tokenizer {
+  val log = Logger.getLogger(getClass().getName())
+
+  def main(argv: Array[String]) {
+    val args = new Conf(argv)
+
+    log.info("Input: " + args.input())
+    log.info("Output: " + args.output())
+    log.info("Number of reducers: " + args.reducers())
+
+    val conf = new SparkConf().setAppName("Compute Bigram Relative Frequency Pairs")
+    val sc = new SparkContext(conf)
+
+    val outputDir = new Path(args.output())
+    FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
+
+    val textFile = sc.textFile(args.input())
+    
+    // Compute the counts of individual words
+    val words = textFile
+      .flatMap(line => tokenize(line))
+      .map(w => (w, 1))
+      .reduceByKey(_ + _)
+
+    // Broadcast the word counts to be used in the frequency computation
+    val word_dict = sc.broadcast(words.collectAsMap())
+    
+    // Compute the bigram frequency
+    val pairs = textFile
+      .flatMap(line => {
+        val tokens = tokenize(line)
+        if (tokens.length > 1) tokens.sliding(2).map(p => p.mkString(", ")).toList else List()
+      })
+      .map(bigram => {
+        val left = bigram.split(", ")(0)
+        val freq = 1f / word_dict.value.get(left).get
+        (bigram, freq)
+      })
+      .reduceByKey(_ + _)
+
+    pairs.saveAsTextFile(args.output())
+    //words.saveAsTextFile("words_output")
+  }
+}
